ec6f912d6447736a32bca26018146af6bd6e0391
Al3xiel <gabriel280104casas@gmail.com>
2024-09-26 02:20:03 -0500

    feat(i18n): added internationalization.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import i18n from "./i18n.js";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+
+app.use(PrimeVue, {
+    ripple: true,
+    theme: {
+        preset: Aura,
+        options: {
+            prefix: 'p',
+            cssLayer: false
+        }
+    }
+});
+
+app.use(i18n);
+
+app.mount('#app');
