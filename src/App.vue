4eae17bb32b22a90c24196f225525474af3c1a12
Al3xiel <gabriel280104casas@gmail.com>
2024-09-26 03:08:09 -0500

    feat(header): Implemented toolbar styles.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script>
 
+import {defineComponent} from "vue";
+import HeaderToolbar from "./public/components/header-toolbar.component.vue";
+
+export default defineComponent({
+  components: {HeaderToolbar}
+})
 </script>
 
 <template>
-
+  <header-toolbar/>
 </template>
 
 <style>
