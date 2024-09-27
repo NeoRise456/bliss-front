b0e0ae9b43364a57df6f59047162700a511422c2
NeoRise456 <neorise456@gmail.com>
2024-09-26 23:34:43 -0500

    feat(model): added service entity.

---
+++ src/client-service/model/service.entity.js
@@ -0,0 +1,15 @@
+export class Service {
+    constructor() {
+        this.id = 0;
+        this.category_id = 0;
+        this.company_id = 0;
+        this.service_name = '';
+        this.description = '';
+        this.price = 0;
+        this.duration = 0;
+        this.raiting = 0;
+        this.sales = 0;
+        this.created_at = '';
+        this.img = '';
+    }
+}
