f8f18cbd154f021441a12c8239dd0f3467083d13
NeoRise456 <neorise456@gmail.com>
2024-09-27 00:53:42 -0500

    feat(service-entity): added service entity.

--- src/client-service/model/service.entity.js
+++ src/client-service/model/service.entity.js
@@ -1,15 +1,27 @@
 export class Service {
-    constructor() {
-        this.id = 0;
-        this.category_id = 0;
-        this.company_id = 0;
-        this.service_name = '';
-        this.description = '';
-        this.price = 0;
-        this.duration = 0;
-        this.raiting = 0;
-        this.sales = 0;
-        this.created_at = '';
-        this.img = '';
+    constructor(
+        id = 0,
+        category_id = 0,
+        company_id = 0,
+        service_name = '',
+        description = '',
+        price = 0,
+        duration = 0,
+        raiting = 0,
+        sales = 0,
+        created_at = new Date(),
+        img = '',
+    ) {
+        this.id = id;
+        this.category_id = category_id;
+        this.company_id = company_id;
+        this.service_name = service_name;
+        this.description = description;
+        this.price = price;
+        this.duration = duration;
+        this.raiting = raiting;
+        this.sales = sales;
+        this.created_at = new Date(created_at);
+        this.img = img;
     }
 }
