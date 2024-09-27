1d017b84ff1427ff3eac007374d1b84cb6239344
NeoRise456 <neorise456@gmail.com>
2024-09-27 00:58:01 -0500

    feat(services-page): added filters and service list.

--- src/client-service/pages/services.component.vue
+++ src/client-service/pages/services.component.vue
@@ -1,11 +1,25 @@
 <script>
+import serviceListComponent from "../components/service-list.component.vue";
+import ServiceList from "../components/service-list.component.vue";
+import PriceFilter from "../components/price-filter.component.vue";
+import CategoryFilter from "../components/category-filter.component.vue";
+
 export default {
-  name: "services"
+  name: "services",
+  components: {CategoryFilter, PriceFilter, ServiceList, serviceListComponent}
 }
 </script>
 
 <template>
-  <p style="color: black">services works</p>
+  <div style="margin: 6rem 0rem; display: flex; ">
+    <div>
+      <category-filter/>
+      <price-filter/>
+    </div>
+    <div>
+      <service-list/>
+    </div>
+  </div>
 </template>
 
 <style>
