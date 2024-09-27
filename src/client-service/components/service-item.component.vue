eb8759aa9dfb85d7c63d69dacd521dd14a822909
NeoRise456 <neorise456@gmail.com>
2024-09-27 00:56:57 -0500

    feat(service-item): service-item component implementation.

--- src/client-service/components/service-item.component.vue
+++ src/client-service/components/service-item.component.vue
@@ -1,11 +1,35 @@
 <script>
+import {Service} from "../model/service.entity.js";
+
 export default {
-  name: "service-item"
+  name: "service-item",
+  props: {
+    service: {
+      type: Service,
+      required: true
+    }
+  }
 }
 </script>
 
 <template>
-
+  <pv-card style="width: 20rem; height: auto;overflow: hidden">
+    <template #header>
+      <img alt="user header" :src="service.img" width="320px"/>
+    </template>
+    <template #title> {{ service.service_name }}</template>
+    <template #subtitle>Price: {{ service.price }}</template>
+    <template #content>
+      <p class="m-0">
+        {{ service.description }}
+      </p>
+    </template>
+    <template #footer>
+      <div class="flex gap-4 mt-1">
+        <pv-button icon="pi pi-arrow-right" icon-pos="right" label="Learn More" class="w-full" />
+      </div>
+    </template>
+  </pv-card>
 </template>
 
 <style scoped>
