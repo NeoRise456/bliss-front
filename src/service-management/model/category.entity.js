8461b6bbca39569d784d92b786da6eae17630271
NeoRise456 <neorise456@gmail.com>
2024-10-16 18:29:34 -0500

    feat(categoy entity): added category entity  .

---
+++ src/service-management/model/category.entity.js
@@ -0,0 +1,13 @@
+
+
+export class Category {
+    constructor(
+        id = 0,
+        category_name = '',
+        description = '',
+    ) {
+        this.id = id;
+        this.category_name = category_name;
+        this.description = description;
+    }
+}
