b5f6045c047a9006700394815c7ed23df7ad4e51
cwassointt <dillpixel1@gmail.com>
2024-10-16 20:39:09 -0500

    feat: added i18n for price filter.

--- src/service-management/components/price-filter.component.vue
+++ src/service-management/components/price-filter.component.vue
@@ -5,7 +5,6 @@ export default {
   name: "price-filter",
   components: {},
   props: {
-    // Props for default min and max values
     min: {
       type: Number,
       default: 0,
@@ -16,41 +15,37 @@ export default {
     }
   },
   setup(props) {
-    // Define reactive values for minValue, maxValue, and current value
     const minValue = ref(props.min);
     const maxValue = ref(props.max);
     const value = ref((minValue.value + maxValue.value) / 2);
 
-    // Watch the value to ensure it stays within the min and max bounds
     watch(value, (newValue) => {
       if (newValue < minValue.value) value.value = minValue.value;
       if (newValue > maxValue.value) value.value = maxValue.value;
     });
 
-    // Return reactive variables to template
-    return {minValue, maxValue, value};
+    return { minValue, maxValue, value };
   }
 };
 </script>
-
 
 <template>
   <div style="width: 40rem; margin: 1rem;">
     <pv-card>
       <template #content>
-        <h1> Filter By Price </h1>
+        <h1>{{ $t('priceFilter.filterByPrice') }}</h1>
 
         <div style="margin: 1rem">
           <pv-input-text
               id="min-input"
               v-model.number="value[0]"
-              placeholder="Min Value"
+              :placeholder="$t('priceFilter.minValue')"
           />
 
           <pv-input-text
               id="max-input"
               v-model.number="value[1]"
-              placeholder="Max Value"
+              :placeholder="$t('priceFilter.maxValue')"
               style="margin-left: 1rem"
           />
         </div>
@@ -65,7 +60,7 @@ export default {
         />
       </template>
       <template #footer>
-        <pv-button label="Filter"/>
+        <pv-button :label="$t('priceFilter.filter')"/>
       </template>
     </pv-card>
   </div>
