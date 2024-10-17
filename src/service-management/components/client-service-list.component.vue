78354d3ed145584b6a4d4edf8e17daace9991113
cwassointt <dillpixel1@gmail.com>
2024-10-16 20:39:50 -0500

    feat: added i18n for client service list component.

--- src/service-management/components/client-service-list.component.vue
+++ src/service-management/components/client-service-list.component.vue
@@ -1,7 +1,7 @@
 <script>
-import {ServicesApiService} from "../services/services-api.service.js";
+import { ServicesApiService } from "../services/services-api.service.js";
 import ServiceItem from "./client-service-item.component.vue";
-import {Service} from "../model/service.entity.js";
+import { Service } from "../model/service.entity.js";
 
 export default {
   name: "service-list",
@@ -22,42 +22,36 @@ export default {
         const serviceApiService = new ServicesApiService();
         const response = await serviceApiService.getServices();
         this.services = response.data.map(service => new Service(
-          service.id,
-          service.category_id,
-          service.company_id,
-          service.service_name,
-          service.description,
-          service.price,
-          service.duration,
-          service.rating,
-          service.sales,
-          service.created_at,
-          service.img
+            service.id,
+            service.category_id,
+            service.company_id,
+            service.service_name,
+            service.description,
+            service.price,
+            service.duration,
+            service.rating,
+            service.sales,
+            service.created_at,
+            service.img
         ));
       } catch (error) {
         console.error('Error fetching services:', error);
       }
     },
   },
-
 }
 </script>
 
 <template>
-
   <div>
-    <h1>Services</h1>
-
+    <h1>{{ $t('clientServiceList.title') }}</h1>
     <div style="display: flex; flex-wrap: wrap; gap: 3rem;">
-      <service-item  v-for="service in services"
-                     :key="service.id"
-                     :service="service"/>
+      <service-item v-for="service in services"
+                    :key="service.id"
+                    :service="service"/>
     </div>
-
-
   </div>
 </template>
 
 <style scoped>
-
 </style>
