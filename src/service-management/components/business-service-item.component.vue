ffccb35546e84ea77037d78db6d7a30238d91d91
cwassointt <dillpixel1@gmail.com>
2024-10-16 20:41:03 -0500

    feat: added i18n for business service item component.

--- src/service-management/components/business-service-item.component.vue
+++ src/service-management/components/business-service-item.component.vue
@@ -1,19 +1,18 @@
 <script>
-import {Service} from "../model/service.entity.js";
-
+import { Service } from "../model/service.entity.js";
 
 export default {
   name: "business-service-item",
   components: {},
-  props:{
-    service:{
+  props: {
+    service: {
       type: Service,
       required: true
     }
   },
-  methods :{
-    redirectToServiceDetail(serviceId){
-      this.$router.push({name:'Business-Service', params:{id:serviceId}});
+  methods: {
+    redirectToServiceDetail(serviceId) {
+      this.$router.push({ name: 'Business-Service', params: { id: serviceId } });
     }
   }
 }
@@ -23,7 +22,7 @@ export default {
   <pv-card class="bg-white item-card">
     <template #header>
       <div style="padding: 10px">
-        <img alt="user header" :src="service.img" width="300px"/>
+        <img alt="user header" :src="service.img" width="300px" />
       </div>
     </template>
     <template #title>
@@ -40,30 +39,29 @@ export default {
       <div class="flex gap-4 mt-1">
         <pv-button icon="pi pi-pencil"
                    icon-pos="right"
-                   label="Edit"
+                   :label="$t('businessService.edit')"
                    class="w-full"
-                   @click="redirectToServiceDetail(service.id)"/>
+                   @click="redirectToServiceDetail(service.id)" />
         <pv-button icon="pi pi-delete-left"
                    icon-pos="right"
-                   label="Delete"
+                   :label="$t('businessService.delete')"
                    class="w-full"
-                   severity="danger"
-                   />
+                   severity="danger" />
       </div>
     </template>
   </pv-card>
 </template>
 
 <style scoped>
-  .item-card{
-    width:20rem;
-    height: auto;
-    overflow:hidden;
-    border-style: solid;
-    border-color: black;
-    border-width: 1px;
-  }
-  .content-text {
-    color: #37123C;
-  }
+.item-card {
+  width: 20rem;
+  height: auto;
+  overflow: hidden;
+  border-style: solid;
+  border-color: black;
+  border-width: 1px;
+}
+.content-text {
+  color: #37123C;
+}
 </style>
