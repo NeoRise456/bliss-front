0a91d03eacdbb354796266bf443050391394a19b
NeoRise456 <neorise456@gmail.com>
2024-10-16 22:09:24 -0500

    feat(price-filter): added emit price range selected.

--- src/service-management/components/price-filter.component.vue
+++ src/service-management/components/price-filter.component.vue
@@ -1,52 +1,58 @@
 <script>
-import { ref, watch } from 'vue';
-
 export default {
   name: "price-filter",
-  components: {},
   props: {
-    // Props for default min and max values
-    min: {
+    minServiceValue: {
       type: Number,
-      default: 0,
+      required: true,
     },
-    max: {
+    maxServiceValue: {
       type: Number,
-      default: 1000,
+      required: true
+    },
+    modelValue: {
+      type: Array,
+      default: []
+    },
+  },
+  emits: ['update:modelValue'],
+  data() {
+    return {
+      minValue: this.minServiceValue,
+      maxValue: this.maxServiceValue,
+      value: [this.minServiceValue, this.maxServiceValue]
+    };
+  },
+  watch: {
+    minServiceValue(newVal) {
+      this.minValue = newVal;
+    },
+    maxServiceValue(newVal) {
+      this.maxValue = newVal;
+    },
+    value(newValue) {
+      if (newValue[0] < this.minValue) newValue[0] = this.minValue;
+      if (newValue[1] > this.maxValue) newValue[1] = this.maxValue;
+      this.$emit('update:modelValue', newValue);
     }
   },
-  setup(props) {
-    // Define reactive values for minValue, maxValue, and current value
-    const minValue = ref(props.min);
-    const maxValue = ref(props.max);
-    const value = ref((minValue.value + maxValue.value) / 2);
-
-    // Watch the value to ensure it stays within the min and max bounds
-    watch(value, (newValue) => {
-      if (newValue < minValue.value) value.value = minValue.value;
-      if (newValue > maxValue.value) value.value = maxValue.value;
-    });
-
-    // Return reactive variables to template
-    return {minValue, maxValue, value};
+  created() {
+    this.value = [this.minServiceValue, this.maxServiceValue];
   }
 };
 </script>
 
-
 <template>
   <div class="w-auto">
     <pv-card>
+      <template #title>Price Filter</template>
       <template #content>
-        <h1> Filter By Price </h1>
-
         <div class="m-3 flex">
           <pv-input-text
               id="min-input"
               v-model.number="value[0]"
               placeholder="Min Value"
           />
-
           <pv-input-text
               id="max-input"
               v-model.number="value[1]"
@@ -54,17 +60,13 @@ export default {
               class="ml-2"
           />
         </div>
-
         <pv-slider
             v-model="value"
             range
             class="w-56 m-1"
-            :min="0"
-            :max="1000"
+            :min="minValue"
+            :max="maxValue"
         />
-      </template>
-      <template #footer>
-        <pv-button label="Filter"/>
       </template>
     </pv-card>
   </div>
