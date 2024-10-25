2c53efdcf9d6ef3ed21050bd1d806b694495b099
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2024-10-25 13:51:55 -0500

    feat(home-best-service):
    add of all services.

--- src/public/components/best-service.component.vue
+++ src/public/components/best-service.component.vue
@@ -1,6 +1,22 @@
 <script>
+import {ServiceApiService} from "../../service-management/services/service-api.service.js";
+
 export default {
-  name: "best-service"
+  name: "best-service",
+  data() {
+    return {
+      services: [],
+    };
+  },
+  async mounted() {
+    const serviceApiService = new ServiceApiService();
+    try{
+      const servicesResponse = await serviceApiService.getServices();
+      this.services = servicesResponse.data;
+    } catch (error) {
+      console.error("Error loading best services: ",error);
+    }
+  }
 }
 </script>
 
