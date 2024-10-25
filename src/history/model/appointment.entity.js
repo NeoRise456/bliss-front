4ffa034195ae2027e64bd7939dc2c21489c09ac5
ElviaRV <elviamarcela14@gmail.com>
2024-10-24 20:07:05 -0500

    fix(history): appointment.entity.js's unused attributes deleted

--- src/history/model/appointment.entity.js
+++ src/history/model/appointment.entity.js
@@ -3,23 +3,19 @@ export class Appointment {
         id = 0,
         userId = 0,
         serviceId = 0,
-        company = 0,
+        companyId = 0,
         reservationDate = '',
         status = '',
-        payment = { amount: 0, method: '', status: '' },
-        schedule = { date: '', time: '' },
-        serviceName = '',
-        companyName = ''
+        date = '',
+        time = '',
     ) {
         this.id = id;
         this.userId = userId;
         this.serviceId = serviceId;
-        this.company = company;
+        this.companyId = companyId;
         this.reservationDate = reservationDate;
         this.status = status;
-        this.payment = payment;
-        this.schedule = schedule;
-        this.serviceName = serviceName;
-        this.companyName = companyName;
+        this.date = date;
+        this.time = time;
     }
 }
