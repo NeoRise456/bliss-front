52aa9cf809a1bfba1ce8c77b960230bd0ed7129d
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2024-10-24 19:50:41 -0500

    feat(home-service-category):
    add category acquisition.

--- src/public/components/service-category.component.vue
+++ src/public/components/service-category.component.vue
@@ -1,6 +1,23 @@
 <script>
+import {CategoryApiService} from "../../service-management/services/category-api.service.js";
+
 export default {
-  name: "service-category"
+  name: "service-category",
+  data(){
+    return {
+      categories: [],
+    };
+  },
+  async mounted(){
+    const categoryApiService = new CategoryApiService();
+    try {
+      const categoriesResponse = await categoryApiService.getCategories();
+      this.categories = categoriesResponse.data;
+    } catch (error) {
+      console.error("Error loading categories or services: ",error);
+    }
+  }
+
 }
 </script>
 
