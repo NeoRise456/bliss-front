d926b5ff5e40ab709b81c5b253871f3d6e035c5d
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2024-10-25 15:20:53 -0500

    feat(home-best-companies):
    add the total sum of the ratings of all the company's services and the count of services offered by each company.

--- src/public/components/best-companies.component.vue
+++ src/public/components/best-companies.component.vue
@@ -6,13 +6,30 @@ export default {
   data(){
     return{
       companies: [],
+      services: [],
     };
   },
   async mounted() {
     const serviceApiService = new ServiceApiService();
     try {
+      const servicesResponse = await serviceApiService.getServices();
       const companiesResponse = await serviceApiService.getCompanies();
-     this.companies = companiesResponse.data;
+
+      this.services = servicesResponse.data;
+      this.companies = companiesResponse.data;
+
+      const companyRatings = {};
+
+      this.services.forEach(service => {
+        if (!companyRatings[service.company_id]) {
+          companyRatings[service.company_id] = {totalRating: 0, serviceCount: 0};
+        }
+
+        companyRatings[service.company_id].totalRating += service.rating;
+        companyRatings[service.company_id].serviceCount++;
+
+      })
+
     } catch (e) {
       console.error(e);
     }
