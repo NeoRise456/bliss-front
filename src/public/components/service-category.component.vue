97e4132429da081be9d625997aa6da8cbcd33e91
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2024-10-24 20:39:54 -0500

    feat(home-service-category):
    Add service template by category.

--- src/public/components/service-category.component.vue
+++ src/public/components/service-category.component.vue
@@ -2,6 +2,7 @@
 import {CategoryApiService} from "../../service-management/services/category-api.service.js";
 import {ServiceApiService} from "../../service-management/services/service-api.service.js";
 
+
 export default {
   name: "service-category",
   data(){
@@ -40,6 +41,31 @@ export default {
 </script>
 
 <template>
+  <div class="p-m-4">
+    <div v-for="category in categories" :key="category.id" class="category-container p-mb-4">
+      <h2 class="p-text-center">{{category.name}}</h2>
+      <div class="services-container p-d-flex p-flex-nowrap p-overflow-x-auto" ref="servicesContainer{{category.id}}">
+        <pv-card
+            v-for="service in servicesByCategory[category.id]"
+            :key="service.id"
+            class="sercice-card p-shadow-2 p-mr-2">
+          <template #header>
+            <img :src="service.img" alt="Service image" class="service-img"/>
+          </template>
+          <template #title>
+            {{service.service_name}}
+          </template>
+          <template #content>
+            <p>{{service.description}}</p>
+            <p class="p-mt-2">{{$t('servicesHome.price')}}: ${{service.price}}</p>
+            <p class="p-mt-2">{{$t('servicesHome.duration')}}: {{service.duration}} min</p>
+            <p class="p-mt-2">{{$t('servicesHome.sales')}}: {{service.sales}}</p>
+            <p class="p-mt-2">{{$t('servicesHome.rating')}}: {{service.rating}}</p>
+          </template>
+        </pv-card>
+      </div>
+    </div>
+  </div>
 
 </template>
 
