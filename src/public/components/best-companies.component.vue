4cc39bb5eee88c3290a8b4bb868fb155758702fc
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2024-10-25 15:13:50 -0500

    feat(home-best-companies):
    add from all companies.

--- src/public/components/best-companies.component.vue
+++ src/public/components/best-companies.component.vue
@@ -1,6 +1,22 @@
 <script>
+import {ServiceApiService} from "../../service-management/services/service-api.service.js";
+
 export default {
-  name: "best-companies"
+  name: "best-companies",
+  data(){
+    return{
+      companies: [],
+    };
+  },
+  async mounted() {
+    const serviceApiService = new ServiceApiService();
+    try {
+      const companiesResponse = await serviceApiService.getCompanies();
+     this.companies = companiesResponse.data;
+    } catch (e) {
+      console.error(e);
+    }
+  }
 }
 </script>
 
