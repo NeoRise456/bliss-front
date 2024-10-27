f859e9f14d832c9fe3bf5d8e701c2296e43f8db0
NeoRise456 <neorise456@gmail.com>
2024-10-27 00:28:10 -0500

    feat(reservations): added methods to api call the service to be booked.

--- src/service-management/pages/reservations.component.vue
+++ src/service-management/pages/reservations.component.vue
@@ -1,13 +1,51 @@
 <script>
+import {ServiceApiService} from "../services/service-api.service.js";
+import {Service} from "../model/service.entity.js";
+
+
+
 export default {
-  name: "reservations"
+  name: "reservations",
+  components: {},
+  data(){
+    return {
+      serviceId: this.$route.params.id,
+      bookingService: Service,
+      serviceApiService: new ServiceApiService(),
+      date: new Date(),
+      time: new Date(),
+    }
+  },
+  methods: {
+    buildServiceFromResponseData(service) {
+      return new Service(
+          service.id,
+          service.category_id,
+          service.company_id,
+          service.service_name,
+          service.description,
+          service.price,
+          service.duration,
+          service.rating,
+          service.sales,
+          service.created_at,
+          service.img
+      )
+    },
+    async getBookingService(){
+      const response = await this.serviceApiService.getServiceById(this.serviceId);
+      this.bookingService = this.buildServiceFromResponseData(response.data);
+    }
+  },
+  created(){
+    this.getBookingService();
+  }
+
 }
 </script>
 
 <template>
-  <div>
-    <h1>Reservations</h1>
-  </div>
+
 </template>
 
 <style scoped>
