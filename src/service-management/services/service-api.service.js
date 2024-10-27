8a6da4f3f319995616c557e7f0bfecac09f3690b
NeoRise456 <neorise456@gmail.com>
2024-10-27 00:24:26 -0500

    refactor: removed unused functions.

--- src/service-management/services/service-api.service.js
+++ src/service-management/services/service-api.service.js
@@ -2,37 +2,6 @@ import http from "../../shared/services/http-common.js";
 
 export class ServiceApiService {
 
-
-    /**
-     * Get a single category by ID
-     * @param id
-     * @returns {Promise<axios.AxiosResponse<any>>}
-     */
-
-    getCategory(id) {
-        return http.get(`/categories/${id}`);
-    }
-
-    /**
-     * Get all companies
-     * @returns {Promise<axios.AxiosResponse<any>>}
-     */
-
-    getCompanies() {
-        return http.get('/companies');
-    }
-
-    /**
-     * Get a single company by ID
-     * @param id
-     * @returns {Promise<axios.AxiosResponse<any>>}
-     */
-
-    getCompany(id) {
-        return http.get(`/companies/${id}`);
-    }
-
-
     /**
      * Get all services
      * @returns {Promise<axios.AxiosResponse<any>>}
@@ -58,7 +27,7 @@ export class ServiceApiService {
      * @returns {Promise<axios.AxiosResponse<any>>}
      */
 
-    getService(id) {
+    getServiceById(id) {
         return http.get(`/services/${id}`);
     }
 
