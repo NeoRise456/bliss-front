b6150b793b08b9fe34cb55e8ca0630918d7632a8
cwassointt <dillpixel1@gmail.com>
2024-10-27 02:01:21 -0500

    fix: removed obsolete fields.

--- src/history/model/appointment.entity.js
+++ src/history/model/appointment.entity.js
@@ -8,24 +8,19 @@ export class Appointment {
         status = '',
         date = '',
         time = '',
-        payment = { amount: 0, method: '', status: '' },
-        schedule = { date: '', time: '' },
-        serviceName = '',
-        companyName = '',
-        review = null// Add review property
+
     ) {
         this.id = id;
-        this.userId = userId;
-        this.serviceId = serviceId;
-        this.companyId = companyId;
-        this.reservationDate = reservationDate;
+        this.user_Id = userId;
+        this.service_Id = serviceId;
+        this.company_Id = companyId;
+        this.reservation_Date = reservationDate;
         this.status = status;
         this.date = date;
         this.time = time;
-        this.payment = payment;
-        this.schedule = schedule;
-        this.serviceName = serviceName;
-        this.companyName = companyName;
-        this.review = review; // Initialize review property
+
     }
 }
+
+
+
