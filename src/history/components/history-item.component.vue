4a421002110c0d0a36846c8cdc03007b2731964f
ElviaRV <elviamarcela14@gmail.com>
2024-10-27 13:16:02 -0500

    feat(history): fixed styles.

--- src/history/components/history-item.component.vue
+++ src/history/components/history-item.component.vue
@@ -35,57 +35,76 @@ const deleteReview = async (reviewId) => {
 
 <template>
   <div class="history-card">
+
     <div class="history-content">
       <h3 class="service-name">{{ appointment.serviceName }}</h3>
       <p class="company-name">{{ appointment.companyName }}</p>
       <p class="appointment-date" v-if="appointment.date">{{ appointment.date }}</p>
       <p class="appointment-date" v-else>No schedule available</p>
-      <div class="card-actions">
-        <button v-if="!review" @click="goToReviewPage(appointment.id)">{{ 'Add Review' }}</button>
-        <div v-if="review">
-          <button @click="goToReviewPage(appointment.id)">{{ 'Edit Review' }}</button>
-          <button @click="deleteReview(review.id)" class="warn">{{ 'Delete Review' }}</button>
-        </div>
+    </div>
+    <div class="card-actions"> <!-- Contenedor para los botones -->
+      <button class="add-button" v-if="!review" @click="goToReviewPage(appointment.id)">{{ 'Add Review' }}</button>
+      <div class="actions-buttons" v-if="review">
+        <button class="edit-button" @click="goToReviewPage(appointment.id)">{{ 'Edit Review' }}</button>
+        <button class="delete-button" @click="deleteReview(review.id)" >{{ 'Delete Review' }}</button>
       </div>
     </div>
   </div>
 </template>
 
+
 <style scoped>
 .history-card {
-  width: 100%;
-  padding: 20px;
+  display: flex;
+  padding: 16px;
   border: 1px solid #ccc;
-  border-radius: 10px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  background-color: white;
+  border-radius: 8px;
+  margin-bottom: 16px;
+  position: relative;
+  align-items: center;
+  max-width: 400px;
+  gap: 20px;
+  background-color: white ;
+
 }
 
 .history-content {
-  display: flex;
-  flex-direction: column;
-  text-align: left;
-}
-
-.service-name {
-  font-size: 1.5rem;
-  color: black;
-  margin: 5px 0;
-}
-
-.company-name, .appointment-date {
-  font-size: 1rem;
-  color: #666;
-  margin: 5px 0;
+  width: 10%;
+  flex-grow: 1;
 }
 
 .card-actions {
   display: flex;
-  gap: 10px;
-  margin-top: 10px;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
 }
 
-button.warn {
+.actions-buttons {
+  justify-content: space-evenly;
+  gap: 20px;
+  display: flex;
+  flex-direction: column;
+}
+
+button {
+  margin-bottom: 8px;
+}
+
+.warn {
+  background-color: white;
+  color: white;
+}
+button.add-button {
+  background-color: #37123C;
+  color: white;
+}
+button.edit-button {
+  background-color: #37123C;
+  color: white;
+}
+
+button.delete-button {
   color: red;
 }
 </style>
--- src/history/pages/history-page.component.vue
+++ src/history/pages/history-page.component.vue
@@ -12,24 +12,33 @@ export default {
 <template>
   <div class="history-page">
     <h1>Completed Appointments</h1>
-    <Suspense>
-      <template #default>
         <HistoryList/>
-      </template>
-      <template #fallback>
-        <div>Loading...</div>
-      </template>
-    </Suspense>
   </div>
 </template>
 
 <style scoped>
+
 .history-page {
-  padding: 20px;
+  margin-top: 0px; /* Resetea el margen superior */
+  padding: 16px;
+  width: 100%;
+  max-width: 800px; /* Controla el ancho máximo del contenido */
+  margin-left: auto;
+  margin-right: auto;
+  position: relative;
 }
 
-h1 {
-  color: #333;
-  margin-bottom: 20px;
+.parent-container {
+  display: flex;
+  flex-direction: column; /* Asegúrate de que el contenido esté en columna */
+  justify-content: flex-start; /* Esto moverá el contenido hacia la parte superior */
+  align-items: center; /* Si deseas centrar horizontalmente */
+  min-height: 100vh; /* Asegúrate de que no se estire al 100% de la altura */
 }
+
+html, body {
+  height: 100%; /* Esto asegura que el body ocupe el 100% del alto de la ventana */
+}
+
+
 </style>
