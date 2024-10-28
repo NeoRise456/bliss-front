1f7ee54cce93b55d39750ef8de8e51aafd65687e
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2024-10-27 23:26:16 -0500

    feat(home-top-rated-reviews):
    add the call for reviews.

--- src/public/components/top-rated-reviews.component.vue
+++ src/public/components/top-rated-reviews.component.vue
@@ -1,6 +1,33 @@
 <script>
+import {ReviewApiService} from "../../review/services/review.service.js";
+
 export default {
-  name: "top-rated-reviews"
+  name: "top-rated-reviews",
+  data(){
+    return {
+      topRatedReviews: []
+    }
+  },
+  methods:{
+    async loadTopRatedReviews(){
+      const reviewApiService= new ReviewApiService();
+      try{
+        const reviewsResponse= await reviewApiService.getReview();
+        const reviews= reviewsResponse.data;
+
+        const sortedReviews= reviews.sort((a,b)=> b.rating - a.rating);
+
+        const topReviews= sortedReviews.slice(0,5);
+
+
+
+      }catch (error){
+        console.error("Error loading top rated reviews:",error);
+      }
+
+    }
+  }
+
 }
 </script>
 
