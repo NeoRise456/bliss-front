6d7476ac1a7ee8886d5c3549a59f6c90ee81cb36
cwassointt <dillpixel1@gmail.com>
2024-11-01 21:59:02 -0500

    fix: fixed image display size to keep style.

--- src/public/components/service-category.component.vue
+++ src/public/components/service-category.component.vue
@@ -1,6 +1,6 @@
 <script>
-import {CategoryApiService} from "../../service-management/services/category-api.service.js";
-import {ServiceApiService} from "../../service-management/services/service-api.service.js";
+import { CategoryApiService } from "../../service-management/services/category-api.service.js";
+import { ServiceApiService } from "../../service-management/services/service-api.service.js";
 
 export default {
   name: "service-category",
@@ -16,69 +16,84 @@ export default {
       const serviceApiService = new ServiceApiService();
 
       try {
-        // Obtención de las categorías
         const categoriesResponse = await categoryApiService.getCategories();
         this.categories = categoriesResponse.data;
 
-        // Obtención de todos los servicios
         const servicesResponse = await serviceApiService.getServices();
         const allServices = servicesResponse.data;
 
-        // Filtrado de los servicios por categoría
         for (const category of this.categories) {
           this.servicesByCategory[category.id] = allServices.filter(
-              service => service.category_id === category.id
+              (service) => service.category_id === category.id
           );
         }
-
       } catch (error) {
         console.error("Error loading categories or services: ", error);
       }
-    }
+    },
   },
   mounted() {
     this.fetchServicesByCategory();
-  }
+  },
 };
-
 </script>
 
 <template>
-  <div class="p-m-4">
-    <div v-for="category in categories" :key="category.id" class="category-container p-mb-4">
-      <h2 class="p-text-center">{{category.category_name}}</h2>
-      <div class="services-container p-d-flex" ref="servicesContainer{{category.id}}">
-        <pv-card
-            v-for="service in servicesByCategory[category.id]"
-            :key="service.id"
-            class="service-card p-shadow-2 p-mr-2"
-        >
-          <template #header>
-            <div style="padding: 10px">
-              <img alt="service image" :src="service.img" width="300px" style="border-radius: 10px;" />
-            </div>
-          </template>
-          <template #title>
-            <div class="content-text">
-            {{service.service_name}}
-            </div>
-          </template>
-          <template #content>
-            <div class="content-text">
-            <p>{{service.description}}</p>
-            <p class="p-mt-2">{{$t('servicesHome.price')}}: ${{service.price}}</p>
-            <p class="p-mt-2">{{$t('servicesHome.duration')}}: {{service.duration}} min</p>
-            <p class="p-mt-2">{{$t('servicesHome.sales')}}: {{service.sales}}</p>
-            <p class="p-mt-2">{{$t('servicesHome.rating')}}: {{service.rating}}</p>
-            </div>
-          </template>
-        </pv-card>
+  <div class="service-category-container">
+    <h2 class="p-text-center title">{{ $t('serviceCategory.title') }}</h2>
+    <div class="p-m-4">
+      <div v-for="category in categories" :key="category.id" class="category-container p-mb-4">
+        <h2 class="p-text-center">{{ category.category_name }}</h2>
+        <div class="services-container p-d-flex" ref="servicesContainer{{ category.id }}">
+          <pv-card
+              v-for="service in servicesByCategory[category.id]"
+              :key="service.id"
+              class="service-card p-shadow-2 p-mr-2"
+          >
+            <template #header>
+              <div style="padding: 10px">
+                <img alt="service image" :src="service.img" class="service-image" />
+              </div>
+            </template>
+            <template #title>
+              <div class="content-text">
+                {{ service.service_name }}
+              </div>
+            </template>
+            <template #content>
+              <div class="content-text">
+                <p>{{ service.description }}</p>
+                <p class="p-mt-2">{{ $t('servicesHome.price') }}: ${{ service.price }}</p>
+                <p class="p-mt-2">{{ $t('servicesHome.duration') }}: {{ service.duration }} min</p>
+                <p class="p-mt-2">{{ $t('servicesHome.sales') }}: {{ service.sales }}</p>
+                <p class="p-mt-2">{{ $t('servicesHome.rating') }}: {{ service.rating }}</p>
+              </div>
+            </template>
+          </pv-card>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+.service-category-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 16px;
+}
+
+.p-text-center {
+  text-align: center;
+  color: #37123C;
+  margin-bottom: 16px;
+}
+
+.title {
+  font-size: 3rem;
+  text-decoration: underline;
+}
 
 .p-m-4 {
   padding: 16px;
@@ -109,9 +124,12 @@ export default {
 .service-card:hover {
   transform: scale(1.05);
 }
-.p-text-center {
-  text-align: center;
-  color: #37123C;
+
+.service-image {
+  width: 100%;
+  height: 200px;
+  object-fit: cover;
+  border-radius: 10px;
 }
 
 .p-mb-4 {
@@ -123,10 +141,10 @@ export default {
 }
 
 .p-shadow-2 {
-  box-shadow: 0px 2px 8px rgb(179, 157, 231);/**/
+  box-shadow: 0px 2px 8px rgb(179, 157, 231);
 }
+
 .content-text {
   color: #37123C;
 }
-
 </style>
