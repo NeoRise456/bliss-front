99a4442038919f789d7cf2c37ad11d3baeb19581
cwassointt <dillpixel1@gmail.com>
2024-11-11 12:50:08 -0500

    feat: added new method to format the date string in business appointments, and changed style of cancel button.

--- src/appointment-management/components/business-appointment-item.component.vue
+++ src/appointment-management/components/business-appointment-item.component.vue
@@ -7,6 +7,11 @@ export default {
       required: true
     }
   },
+  computed: {
+    formattedDate() {
+      return new Date(this.user.date).toLocaleDateString('en-CA'); // Formato "YYYY-MM-DD"
+    }
+  },
   methods: {
     openCancelDialog() {
       this.$emit('open-cancel-dialog', this.user);
@@ -22,7 +27,7 @@ export default {
       <div class="user-details">
         <div class="appointment-info">
           <h3 class="section-title">{{ $t('businessAppointment.appointment') }}</h3>
-          <p><strong>{{ $t('businessAppointment.date') }}:</strong> {{ user.date }}</p>
+          <p><strong>{{ $t('businessAppointment.date') }}:</strong> {{ formattedDate }}</p>
           <p><strong>{{ $t('businessAppointment.time') }}:</strong> {{ user.time }}</p>
         </div>
         <hr class="divider" />
@@ -40,10 +45,10 @@ export default {
           <p><strong>{{ $t('businessAppointment.phone') }}:</strong> {{ user.userPhone }}</p>
           <p><strong>{{ $t('businessAppointment.address') }}:</strong> {{ user.userAddress }}</p>
         </div>
-        <div class="button-container">
-          <button @click.stop="openCancelDialog" class="cancel-button">{{ $t('businessAppointment.cancelAppointment') }}</button>
-        </div>
       </div>
+    </div>
+    <div class="button-container">
+      <button @click.stop="openCancelDialog" class="cancel-button">{{ $t('businessAppointment.cancelAppointment') }}</button>
     </div>
   </div>
 </template>
