b2c676e0dc2d4b8a70a16e66b4901ff1037ba90b
Al3xiel <gabriel280104casas@gmail.com>
2024-11-20 15:54:30 -0500

    fix(cors): enable cors.

--- src/shared/services/http-common.js
+++ src/shared/services/http-common.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import {authenticationInterceptor} from "../../iam/services/authentication.interceptor.js";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
@@ -10,7 +9,5 @@ const http = axios.create({
 http.defaults.headers.common["Content-Type"] = "application/json";
 http.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
 
-// Add a request interceptor
-http.interceptors.request.use(authenticationInterceptor);
 
 export default http;
